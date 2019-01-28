660859a55476741f46f29414346ad7737ec95e03
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 19:06:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,20 +11,30 @@
 #{}```
 
 def dictionary()
-  replaceHash = {}
-  replaceHash['hello'] = 'hi'
-  replaceHash[['to','two','too']] = '2'
-  replaceHash[['for','four']] = '4'
-  replaceHash['be']='b'
-  replaceHash['you']='u'
-  replaceHash['at']='@'
-  replaceHash['and']='&'
-  replaceHash
+
+ replacements = {
+      "hello" => "hi",
+      "to" => "2",
+      "two" => "2",
+      "too" => "2",
+      "for" => "4",
+      "four" => "4",
+      "be" => "b",
+      "you" => "u",
+      "at" => "@",
+      "and" => "&"
+    }
 end
 
 def word_substituter(string)
+holdingarr = string.split(" ")
+holdingarr.map do |word|
+  replacements.each do |key, value|
+    word = value if word.downcase == key
+  end
+  word
+end.join(" ")
 
-  puts string.split(" ")
 end
 newstring = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 word_substituter(newstring)
