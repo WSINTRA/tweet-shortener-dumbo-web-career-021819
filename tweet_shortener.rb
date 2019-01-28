46093395d4a9aa50df0d0f94c05a2eb2c0b0d6ba
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 19:42:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -52,5 +52,5 @@ def selective_tweet_shortener(tweets)
   end
 end
 
-tweetArr = ["GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!","Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"]
-selective_tweet_shortener(tweetArr)
+#tweetArr = ["GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!","Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"]
+#selective_tweet_shortener(tweetArr)
