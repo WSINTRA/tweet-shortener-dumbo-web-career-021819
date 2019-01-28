8021a19592d659f301419be0d1cca06cc2923d20
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 19:56:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -63,4 +63,4 @@ end
 
 
 string = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
-shortened_tweet_truncator(string)
+puts shortened_tweet_truncator(string)
