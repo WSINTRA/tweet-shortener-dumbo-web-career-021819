6feaba44207f56c84135aa4810d40270dd4e7c4e
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 19:56:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -62,5 +62,5 @@ end
 
 
 
-#tweetArr = ["GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!","Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"]
-#selective_tweet_shortener(tweetArr)
+string = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
+shortened_tweet_truncator(string)
