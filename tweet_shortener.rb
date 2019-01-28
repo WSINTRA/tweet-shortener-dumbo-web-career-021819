1cd0d5953733c8b8ade852f6ec9eb6b9c5092493
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 19:00:45 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -22,4 +22,9 @@ def dictionary()
   replaceHash
 end
 
-puts dictionary
+def word_substituter(string)
+  string.split
+  puts string
+end
+newstring = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+word_substituter(newstring)
