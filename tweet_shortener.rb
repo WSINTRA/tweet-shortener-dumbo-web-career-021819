e27e66d69ba524b6a5a1aaf4c879411a49c10884
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 19:40:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,11 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweets)
-  puts tweets
+  tweets.each do |x|
+    if x.length > 140
+      puts word_substituter(x)
+    end
+  end
 end
 
 tweetArr = ["GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!","Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"]
