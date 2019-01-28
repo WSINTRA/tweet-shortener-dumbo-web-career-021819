00e084fa125e433a7138d39333bac28e2ab641cf
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 20:00:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -53,7 +53,9 @@ end
 
 def shortened_tweet_truncator(tweets)
   if word_substituter(tweets).length > 140
-    tweets[0,140]
+    outString = tweets[0,137]
+    outString +"..."
+    outString
   else
     word_substituter(tweets)
   end
